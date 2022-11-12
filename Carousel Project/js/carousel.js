0e2336506fb5adecbb73b13ec189ca2db368719d
Jude Odika <judeodika@gmail.com>
2022-11-12 10:43:35 +0100

    Update carousel.js

--- Carousel Project/js/carousel.js
+++ Carousel Project/js/carousel.js
@@ -22,14 +22,31 @@ const updateDots = (currentDot, targetDot) => {
     targetDot.classList.add('current-slide');
 }
 
+const hideShowArrows = (slides, targetIndex, prevButton, nextButton) => {
+    
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+
+    } else if (targetIndex === slides.length - 1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+}
+
 prevButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
     const prevSlide = currentSlide.previousElementSibling;
     const currentDot = dotsNav.querySelector('.current-slide');
     const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
     
     moveToSlide(track, currentSlide, prevSlide);
     updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevIndex, prevButton, nextButton);
 })
 
 nextButton.addEventListener('click', e => {
@@ -37,9 +54,11 @@ nextButton.addEventListener('click', e => {
     const nextSlide = currentSlide.nextElementSibling;
     const currentDot = dotsNav.querySelector('.current-slide');
     const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
     
     moveToSlide(track, currentSlide, nextSlide);
     updateDots(currentDot, nextDot);
+    hideShowArrows(slides, nextIndex, prevButton, nextButton);
 })
 
 dotsNav.addEventListener('click', e => {
@@ -54,6 +73,7 @@ dotsNav.addEventListener('click', e => {
 
     moveToSlide(track, currentSlide, targetSlide);
     updateDots(currentDot, targetDot);
+    hideShowArrows(slides, targetIndex, prevButton, nextButton);
 })
 
 
